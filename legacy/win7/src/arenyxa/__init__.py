d99d6a6dddf9419804ddfa7fb740b9e3d35d20d7
Jerry0408 <wangyixuan.2013.4@proton.me>
2026-09-26 23:35:15 +0800

    release: align legacy lane with public v0.1

--- legacy/win7/src/arenyxa/__init__.py
+++ legacy/win7/src/arenyxa/__init__.py
@@ -1,9 +1,14 @@
 
-__version__ = "8.2"
-__package_version__ = "8.2.0"
+# Public release identity. The Win7 lane shares the public version while
+# retaining the same internal engineering and compatibility baselines.
+__version__ = "0.1"
+__package_version__ = "0.1.0"
+__public_version__ = "0.1"
+__public_package_version__ = "0.1.0"
 __compat_version__ = "6.8.0"
 __release_channel__ = "stable"
 __engineering_build__ = "v8.2.0"
-__display_version__ = "8.2.0"
-__distribution_version__ = "8.2.0"
+__internal_version__ = "8.2.0"
+__display_version__ = "0.1"
+__distribution_version__ = "0.1.0"
 __release_phase__ = 8
